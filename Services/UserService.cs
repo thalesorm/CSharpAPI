9b0fa2b987f740abff016962362cb75c4b209dd1
thalesorm <thalis.mraz@gmail.com>
2024-10-10 10:06:06 -0300

    :construction: feat: service crud user

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,6 +1,17 @@
-﻿namespace ApiGap.Services
+﻿using ApiGap.Data;
+using ApiGap.Interfaces;
+using UserModel = ApiGap.Models;
+
+namespace ApiGap.Services
+
 {
     public class UserService
     {
+        private readonly GapApiDBContext _dbContext;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
     }
 }
